065bde60d80d58cfe7508d191f9f661b60eb7236
Nikos Chondros <nikos@veridise.com>
2025-10-10 15:47:42 +0300

    Simplified further

--- .github/scripts/run-picus.sh
+++ .github/scripts/run-picus.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!//usr/bin/env bash
 
 set -e
 
@@ -8,22 +8,12 @@ VERSION_ID=$2
 
 # Launching AuditHub task
 task_id=$(ah start-picus-v2-task --version-id ${VERSION_ID} --source ${PICUS_FILE} --solver z3)
+
+echo "task_id=$task_id"
+
 # This is mainly for debugging, will let you inspect the logs.
 ah monitor-task --task-id $task_id
 
-# Retrieve all metadata from the task.
-task_info_json=$(ah get-task-info --task-id $task_id | grep -v "^+")
-
-# Check if Picus found counterexamples
-PICUS_CEXES=$(echo "$task_info_json" | jq '.findings_counters.critical')
-if ((${PICUS_CEXES} != 0)); then
-    echo "Picus found counterexample(s), check the logs for details."
-    exit 1
-fi
-
-# Check if Picus hit a time out.
-PICUS_COMPLETED=$(echo "$task_info_json" | jq '.steps[] | select(.code == "run-picus") | .completed_without_timeout')
-if [[ ${PICUS_COMPLETED} == "false" ]]; then
-    echo "Picus was unable to verify ${PICUS_FILE} due to timeout, check the logs for details."
-    exit 1
-fi
+# This will check for counterexamples (and exit with 1) and a potential timeout (and exit with 2)
+# Since it is the last command of the script, its exit code will become the script's exit code
+ah get-task-info --task-id $task_id --output none --verify --check_completed
