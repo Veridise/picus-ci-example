57b093ff16c31ae21036b11c953ccf574744d3b6
Kostas Ferles <kferles@gmail.com>
2025-10-18 15:52:16 -0500

    Simplify AH script.

--- .github/scripts/run-picus.sh
+++ .github/scripts/run-picus.sh
@@ -13,23 +13,17 @@ task_id=$(ah start-picus-v2-task --version-id ${VERSION_ID} --source ${PICUS_FIL
 # This is mainly for debugging, will let you inspect the logs.
 ah monitor-task --task-id $task_id
 
-# Retreive all metadata from the task.
-task_info_json=$(ah get-task-info --task-id $task_id | grep -v "^+")
-
-# Download findings json.
-JSON_FILE=./${TIMESTAMP}-findings.json
-picus_findings_id=$(echo "$task_info_json" | jq -r '.artifacts[] | select(.name == "findings.json") | .id')
-ah get-task-artifact --task-id $task_id --artifact-id $picus_findings_id --output-file ${JSON_FILE}
+ah download-artifact --task-id $task_id --step-code run-picus --name findings.json --output-file ./${PICUS_FILE}-findings.json
 
 # Check if Picus hit a time out or produced unknown findings.
-PICUS_COMPLETED=$(jq '.completed' ${JSON_FILE})
+PICUS_COMPLETED=$(jq '.completed' ./${PICUS_FILE}-findings.json)
 
 if [[ ${PICUS_COMPLETED} == "false" ]]; then
     echo "Picus was unable to verify ${PICUS_FILE}, check the logs for details."
     exit 1
 fi
 
-PICUS_CEXES=$(jq '.findings.critical' ${JSON_FILE})
+PICUS_CEXES=$(jq '.findings.critical' ./${PICUS_FILE}-findings.json)
 
 if ((${PICUS_CEXES} != 0)); then
     echo "Picus found a counterexample, check the logs for details."
